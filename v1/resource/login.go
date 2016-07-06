1bacee4c2ae52705bb14000d488d365fb193681e
Tomoya AMACHI <tomoya.amachi@gmail.com>
2016-07-06 18:36:52 +0900

    Prepare mysql session store

--- v1/resource/login.go
+++ v1/resource/login.go
@@ -11,5 +11,13 @@ func Login(c echo.Context) error {
 	token := c.FormValue("token")
 	appName := c.FormValue("app_code")
 	account := model.Authenticate(token, appName)
+	if account.Id == 0 {
+		response := map[string]interface{}{
+			"statusCode": http.StatusMethodNotAllowed,
+			"message":    "Not authorized user!!",
+		}
+		return c.JSON(http.StatusMethodNotAllowed, response)
+	}
+
 	return c.JSON(http.StatusOK, account)
 }
